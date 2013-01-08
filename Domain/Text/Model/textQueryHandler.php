eaaf4268e6199408462b2312e7f9b812210093dc
mmandt <michael.mandt@logic-works.de>
2013-01-08 15:09:08 +0100

    textCommandHandler + textQueryHandler functions added

--- Domain/Text/Model/textQueryHandler.php
+++ Domain/Text/Model/textQueryHandler.php
@@ -8,11 +8,63 @@ class textQueryHandler
     public function __construct()
     {
         $this->db = lw_registry::getInstance()->getEntry('db');
+        $this->setLanguage("de");
     }
     
-    public function getAllTextsByCategory()
+    public function getAllTextsByCategory($category)
     {
-        $this->db->setStatement("SELECT * FROM t:tabelle DESC ");
+        if (!$this->categoryExists($category)) {
+            throw new Exception('...');
+        }else{
+            $this->db->setStatement("SELECT * FROM t:fab_text WHERE category = :category ");
+            $this->db->bindParameter("category", "s", $category);
+            return $this->db->pselect();
+        }
+    }
+    
+    public function getAllUniqueCategories()
+    {
+        $this->db->setStatement("SELECT DISTINCT category FROM t:fab_text ");
         return $this->db->pselect();
+    }
+    
+    public function getAllUniqueLanguages()
+    {
+        $this->db->setStatement("SELECT DISTINCT language FROM t:fab_text ");
+        return $this->db->pselect();
+    }
+    
+    public function languageExists($lang)
+    {
+        $languages = $this->getAllUniqueLanguages();
+        if(in_array($lang, $languages)){
+            return true;
+        }else{
+            return false;
+        }       
+    }
+    
+    public function categoryExists($category)
+    {
+        $categories = $this->getAllUniqueCategories();
+        if(in_array($category, $categories)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public function getTextById($id)
+    {
+        $this->db->setStatement("SELECT * FROM t:fab_text WHERE id = :id ");
+        $this->db->bindParameter("id", "i", $id);
+        return $this->db->pselect1();
+    }
+    
+    public function setLanguage($lang)
+    {
+        if ($this->languageExists($lang)) {
+            $this->lang = $lang;
+        }
     }
 }
