ad5388c62014a6422eb6af1058f913fd05ed72ea
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-01-08 16:15:16 +0100

    initial commit

---
+++ Library/fabSession.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fab\Library;
+
+class fabSession 
+{
+    public function __construct()
+    {
+        
+    }
+    
+    public function getEmail()
+    {
+        return "arno.nym@fz-juelich.de";
+    }
+    
+    public function getName()
+    {
+        return "Arno Nym";
+    }
+    
+}
