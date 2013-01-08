875bd86062e1039f5cc9bbd6ad1233cb31a71539
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-01-08 10:24:03 +0100

    work in progress

--- Domain/Text/Model/textQueryHandler.php
+++ Domain/Text/Model/textQueryHandler.php
@@ -8,11 +8,40 @@ class textQueryHandler
     public function __construct()
     {
         $this->db = lw_registry::getInstance()->getEntry('db');
+        $this->setLanguage("de");
     }
     
-    public function getAllTextsByCategory()
+    public function getAllTextsByCategory($category)
     {
-        $this->db->setStatement("SELECT * FROM t:tabelle DESC ");
-        return $this->db->pselect();
+        if (!$this->categoryExists($lang)) {
+            throw new Exception('...');
+        }
+    }
+    
+    public function getAllUniqueCategories()
+    {
+        
+    }
+    
+    public function getAllUniqueLanguages()
+    {
+        
+    }
+    
+    public function languageExists($lang)
+    {
+        
+    }
+    
+    public function categoryExists($lang)
+    {
+        
+    }
+    
+    public function setLanguage($lang)
+    {
+        if ($this->languageExists($lang)) {
+            $this->lang = $lang;
+        }
     }
 }
