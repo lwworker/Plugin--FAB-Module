b2de5d28e0e5f56c4c1416a34416772f81e5806b
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-01-10 12:55:47 +0100

    initial commit

---
+++ Library/fabDIC.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Fab\Library;
+
+class fabDIC 
+{
+    public function __construct()
+    {
+        
+    }
+    
+    public function getEventQueryHandler()
+    {
+        if (!$this->eventQueryHandler) {
+            $this->eventQueryHandler = new \Fab\Domain\Event\Model\eventQueryHandler($this->getDbObject());
+        }
+        return $this->eventQueryHandler;        
+    }
+    
+    public function getEventCommandHandler()
+    {
+        if (!$this->eventCommandHandler) {
+            $this->eventCommandHandler = new \Fab\Domain\Event\Model\eventCommandHandler($this->getDbObject());
+        }
+        return $this->eventCommandHandler;        
+    }
+    
+    public function getDbObject()
+    {
+        return \lw_registry::getInstance()->getEntry("db");
+    }
+    
+    public function getEventValidationObject()
+    {
+        $eventValidationSevice = new \Fab\Domain\Event\Service\eventValidate();
+        $eventValidationSevice->setQueryHandler($this->getEventQueryHandler());
+        return $eventValidationSevice;
+    }
+    
+    public function getEventFilter()
+    {
+        return \Fab\Domain\Event\Service\eventFilter::getInstance();
+    }
+    
+    
+}
