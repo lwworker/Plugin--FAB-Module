717a89e7f368e6fc9e9fba1534e26536ace34f73
lwworker <lwworker@logic-works.de>
2013-01-14 11:05:21 +0100

    more buttons and list backend styling

--- fab_frontend/fab_frontend.class.php
+++ fab_frontend/fab_frontend.class.php
@@ -6,7 +6,7 @@ class fab_frontend extends lw_plugin
     public function __construct()
     {
         lw_plugin::__construct();
-        $this->response->addHeaderItems('cssfile', $this->config['url']['client'].'assets/css/bootstrap.min.css');
+        $this->response->addHeaderItems('cssfile', $this->config['url']['client'].'assets/css/bootstrap.css');
         $this->response->addHeaderItems('jsfile', $this->config['url']['client'].'assets/js/jquery-1.8.3.min.js');
         $this->response->addHeaderItems('jsfile', $this->config['url']['client'].'assets/js/bootstrap.min.js');
     }
